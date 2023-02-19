2c7c991daf019e460341d1c09c98a0f4bc4f7819
Neeraj Sharma <neerajkumarsharma279@gmail.com>
2023-02-18 22:04:00 -0500

    textutils

---
+++ React_Course/textutils/src/App.js
@@ -0,0 +1,23 @@
+//import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <>
+    <nav>
+      <li>Home</li>
+      <li>About</li>
+      <li>Contact</li>
+      
+    </nav>
+    <div className="blank">Lovely</div>
+    <div className="container">
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nobis soluta dignissimos nostrum consequuntur quidem, praesentium non a obcaecati temporibus nam quas, cupiditate perspiciatis enim asperiores modi. Sit ipsa omnis suscipit ut blanditiis ducimus quia!</p>
+    </div>
+    </>
+    
+
+  );
+}
+
+export default App;
