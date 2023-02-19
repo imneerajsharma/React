9aa20526e970cc85b6686bf3fe62c3d635a86ccd
Neeraj Sharma <neerajkumarsharma279@gmail.com>
2023-02-19 15:43:48 -0500

    Adding more Logic to TextUtils

--- React_Course/textutils/src/components/TextForm.js
+++ React_Course/textutils/src/components/TextForm.js
@@ -6,6 +6,16 @@ export default function TextForm(props) {
         let newText=text.toUpperCase();
         setText(newText);
     }
+    const handleLoClick =() =>{
+        
+        let newText=text.toLowerCase();
+        setText(newText);
+    }
+    const handleClearClick =() =>{
+        
+        let newText=text.toLowerCase();
+        setText(newText);
+    }
     const handleOnChange =(event) =>{
         // console.log(" handleOnChange ");
         setText(event.target.value)
@@ -17,13 +27,29 @@ export default function TextForm(props) {
 
 
   return (
-    <div>
+    <>
+    <div className="container">
         <h1>{props.heading}</h1>
         <div className="mb-3">
         
         <textarea className="form-control" value={text} onChange={handleOnChange} id="myBox" rows="8"></textarea>
+       
         </div>
-        <button className="btn btn-primary" onClick={handleUpClick}>Convert to UpperCase</button>
+        <button className="btn btn-primary mx-2" onClick={handleUpClick}>Convert to UpperCase</button>
+        <button className="btn btn-primary mx-2" onClick={handleLoClick}>Convert to LowerCase</button>
+        <button className="btn btn-primary mx-2" onClick={handleClearClick}>Clear</button>
     </div>
+    <div className="container my-3">
+        <h2>Your Text Summary</h2>
+        <p>{text.split(" ").length} Words and {text.length} Characters</p>
+        <p>{0.008*text.split(" ").length} Minutes to ReadWords</p>
+        <h2>Preview</h2>
+        <p>{text}</p>
+    </div>
+
+    
+    </>
+    
+
   )
 }
