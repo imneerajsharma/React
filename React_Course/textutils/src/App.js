1535d80b513fc3eca7924193309808124f7b4e89
Neeraj Sharma <neerajkumarsharma279@gmail.com>
2023-02-19 01:06:40 -0500

    Understanding State & Handling Events in React

--- React_Course/textutils/src/App.js
+++ React_Course/textutils/src/App.js
@@ -1,18 +1,22 @@
 //import logo from './logo.svg';
 import './App.css';
 import Navbar from './components/Navbar';
+import TextForm from './components/TextForm';
 
 
 function App() {
   return (
-<>
-    
-<Navbar title="TextUtils" aboutText="About TEXT"/>
+          <>
+              
+            <Navbar title="TextUtils" aboutText="About TEXT"/>
+            <div className="container my-3">
+              <TextForm heading="Enter your text to analyse below"/>
+            </div>
 
-    
- 
-</>
-    
+              
+          
+          </>
+              
 
   );
 }
