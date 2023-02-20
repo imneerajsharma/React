c3f7262e183bba1c20925782baf51910ce36c32e
Neeraj Sharma <neerajkumarsharma279@gmail.com>
2023-02-19 22:41:53 -0500

    Dark Mode
    Improving Dark Mode and Refactoring App Component

--- React_Course/textutils/src/App.js
+++ React_Course/textutils/src/App.js
@@ -1,18 +1,35 @@
 //import logo from './logo.svg';
 import './App.css';
-import About from './components/About';
+// import About from './components/About';
 import Navbar from './components/Navbar';
+import React, { useState} from 'react';
+import TextForm from './components/TextForm';
+
 // import TextForm from './components/TextForm';
 
 
 function App() {
+  const [mode, setMode] = useState('light'); //Whether Dark mode is enabled or not
+  
+  const toggleMode=()=>{
+    if(mode==='light'){
+    setMode('dark');
+    document.body.style.backgroundColor='#424344'
+  }
+  else {
+    setMode('light');
+    document.body.style.backgroundColor='white'
+  }
+}
+
   return (
           <>
               
-            <Navbar title="TextUtils" aboutText="About TEXT"/>
+            {/* <Navbar title="TextUtils" aboutText="About TEXT" mode={mode}/> */}
+            <Navbar title="TextUtils" mode={mode} toggleMode={toggleMode}/>
             <div className="container my-3">
-              {/* <TextForm heading="Enter your text to analyse below"/> */}
-              <About/>
+              <TextForm heading="Enter your text to analyze below" mode={mode}/>
+              {/* <About/> */}
             </div>
 
               
--- React_Course/textutils/src/components/TextForm.js
+++ React_Course/textutils/src/components/TextForm.js
@@ -41,18 +41,18 @@ export default function TextForm(props) {
         setText(event.target.value)
     }
 
-    const[text,setText]=useState('Enter text here');
+    const[text,setText]=useState('');
     // text = "new text"; // Wrong way to change the state
     // setText("new text"); // Correct way to change the state
 
 
   return (
     <>
-    <div className="container">
+    <div className="container" style={{color:props.mode==='dark'?'white':'black'}}>
         <h1>{props.heading}</h1>
         <div className="mb-3">
         
-        <textarea className="form-control" value={text} onChange={handleOnChange} id="myBox" rows="8"></textarea>
+        <textarea className="form-control" value={text} onChange={handleOnChange} style={{backgroundColor:props.mode==='dark'?'grey':'white',color:props.mode==='dark'?'white':'black'}}id="myBox" rows="8"></textarea>
        
         </div>
         <button className="btn btn-primary mx-2" onClick={handleUpClick}>Convert to UpperCase</button>
@@ -63,12 +63,13 @@ export default function TextForm(props) {
         
         
     </div>
-    <div className="container my-3">
+    <div className="container my-3 " style={{color:props.mode==='dark'?'white':'black'}}>
         <h2>Your Text Summary</h2>
         <p>{text.split(" ").length} Words and {text.length} Characters</p>
         <p>{0.008*text.split(" ").length} Minutes to ReadWords</p>
         <h2>Preview</h2>
-        <p>{text}</p>
+        {/* <p>{text}</p> */}
+        <p>{text.length>0?text:"Enter something in the textbox above to preview it here"}</p>
     </div>
 
     
