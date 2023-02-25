007b21883ddd4e8fbec7bcbf0da7a01e5aef6742
Neeraj Sharma <neerajkumarsharma279@gmail.com>
2023-02-24 22:40:10 -0500

    Category wise News
    Fetching News category wise in NewsFox

--- React_Course/newsapp/src/App.js
+++ React_Course/newsapp/src/App.js
@@ -31,13 +31,14 @@ export default class App extends Component {
           <Route path="/science"><News pageSize={5} country="in" category="science" /></Route>
           <Route path="/sports"><News pageSize={5} country="in" category="sports" /></Route> */}
 
-          <Route path='/' element={<News pageSize={12} country='us' category='general' />}></Route>
-            <Route path='/business' element={<News pageSize={12} country='us' category='business' />}></Route>
-            <Route path='/entertainment' element={<News pageSize={12} country='us' category='entertainment' />}></Route>
-            <Route path='/health' element={<News pageSize={12} country='us' category='health' />}></Route>
-            <Route path='/science' element={<News  pageSize={12} country='us' category='science' />}></Route>
-            <Route path='/sports' element={<News  pageSize={12} country='us' category='sports' />}></Route>
-            <Route path='/technology' element={<News  pageSize={12} country='us' category='technology' />}></Route>
+          <Route exact path='/' element={<News key='/' pageSize={12} country='us' category='/' />}></Route>
+            <Route exact path='/business' element={<News key='business' pageSize={12} country='us' category='business' />}></Route>
+            <Route exact path='/general' element={<News key='general' pageSize={12} country='us' category='general' />}></Route>
+            <Route exact path='/entertainment' element={<News key='entertainment' pageSize={12} country='us' category='entertainment' />}></Route>
+            <Route exact path='/health' element={<News key='health' pageSize={12} country='us' category='health' />}></Route>
+            <Route exact path='/science' element={<News key='science' pageSize={12} country='us' category='science' />}></Route>
+            <Route exact path='/sports' element={<News key='sports' pageSize={12} country='us' category='sports' />}></Route>
+            <Route exact path='/technology' element={<News key='technology' pageSize={12} country='us' category='technology' />}></Route>
           
         </Routes>
 
