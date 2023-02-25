ec9a5c6a1d5afc523361a762c688a93627c457ec
Neeraj Sharma <neerajkumarsharma279@gmail.com>
2023-02-25 15:30:43 -0500

    Routes & Schema Created for Storing Notes and Users

--- React_Course/inotebook/backend/index.js
+++ React_Course/inotebook/backend/index.js
@@ -5,9 +5,9 @@ connectToMongo();
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => {
-  res.send('Hello Neeraj!')
-})
+//Available Routes
+app.use('/api/auth',require('./routes/auth'))
+app.use('/api/notes',require('./routes/notes'))
 
 
 app.listen(port, () => {
