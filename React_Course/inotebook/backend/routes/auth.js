49ec06816ee199bb4d2e076690246bed25bd93db
Neeraj Sharma <neerajkumarsharma279@gmail.com>
2023-02-26 00:01:30 -0500

    Implementing bcryptjs and JWT for password hashing in Node JS
    This commit implements password hashing using bcryptjs and JSON Web Token (JWT) in Node JS. The bcryptjs library is used to securely hash passwords before storing them in the database. This implementation ensures that users' passwords are not stored in plaintext, providing an extra layer of security. Additionally, JWT is implemented to generate an authentication token upon successful login. This token is then used to authenticate subsequent requests from the user, improving the security of the application. Overall, this implementation enhances the security of the application's password storage and user authentication process.

--- React_Course/inotebook/backend/routes/auth.js
+++ React_Course/inotebook/backend/routes/auth.js
@@ -2,6 +2,10 @@ const express =require('express');
 const router =express.Router();
 const User= require('../models/User');
 const { body, validationResult } = require('express-validator');
+const bcrypt =require('bcryptjs');
+var jwt = require('jsonwebtoken');
+
+const JWT_SECRET = 'Neerajisagood$oy';
 
 //Create a User using :POST "/api/auth/createuser" . No Login required
 router.post('/createuser',[
@@ -23,17 +27,30 @@ router.post('/createuser',[
     if(user){
         return res.status(400).json({error:"Sorry a user with this email already exists"})
     }
-
+        const salt = await bcrypt.genSalt(10);
+        const secPass= await bcrypt.hash(req.body.password,salt)
+    // Create a new user
      user = await User.create({
         name: req.body.name,
         email: req.body.email,
-        password: req.body.password,
-      })
+        password: secPass,
+      });
       
+      const data={
+        user:{
+            is:user.id
+        }
+      }
+      const authtoken = jwt.sign(data,JWT_SECRET);
+
+    //   console.log(jwtData);
+      //res.json(user)
+      res.json({authtoken})
+
     //   .then(user => res.json(user))
     //   .catch(err=>{console.log(err)
     // res.json({error:"Please enter a unique value for email",message:err.message})})
-    res.json(user)
+    
     } 
     catch(error){
         console.error(error.message);
