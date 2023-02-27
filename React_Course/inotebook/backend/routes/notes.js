ab7cc7a8e6e419fd018ad6964ebb18fc3ed60462
Neeraj Sharma <neerajkumarsharma279@gmail.com>
2023-02-26 19:46:50 -0500

    Implement fetching and adding notes functionality
    Implemented the functionality to fetch all notes and add a new note to the database. The API endpoints for fetching and adding notes have been added to the server routes. The GET endpoint for fetching all notes returns an array of note objects with their respective properties, including title, content, and date of creation. The POST endpoint for adding a new note accepts a JSON payload containing the title and content of the note, which is then added to the database with a unique ID and a timestamp. These endpoints are now accessible through the client-side application and will enable users to manage their notes effectively.

--- React_Course/inotebook/backend/routes/notes.js
+++ React_Course/inotebook/backend/routes/notes.js
@@ -1,14 +1,56 @@
-const express =require('express');
-const router =express.Router();
+const express = require("express");
+const router = express.Router();
+var fetchuser = require("../middleware/fetchuser");
+const Notes = require("../models/Notes");
+const { body, validationResult } = require("express-validator");
 
-router.get('/',(req,res)=>{
-  
-    res.json([])
-})
+// Route 1:  Get All the notes using :GET "/api/auth/auth/getuser" . Login required
+router.get("/fetchallnotes", fetchuser, async (req, res) => {
+  try {
+    const notes = await Notes.find({ user: req.user.id });
+    res.json(notes);
+  } catch (error) {
+    console.error(error.message);
+    res.status(500).send("Internal Server Error");
+  }
 
+  // res.json([])
+});
 
+// Route 2: Add a new Note using :POST "/api/auth/addnote" . Login required
+router.post(
+  "/addnote",
+  fetchuser,
+  [
+    body("title", "Enter a valid title").isLength({ min: 3 }),
+    body("description", "password must be atleast 5 characters").isLength({
+      min: 5,
+    }),
+  ],
+  async (req, res) => {
+    try {
+      const { title, description, tag } = req.body;
+      // If there are errors , return the Bad Request and the errors
+      const errors = validationResult(req);
+      if (!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() });
+      }
 
+      const note = new Notes({
+        title,
+        description,
+        tag,
+        user: req.user.id,
+      });
+      const savedNote = await note.save();
+      res.json(savedNote);
+    } catch (error) {
+      console.error(error.message);
+      res.status(500).send("Internal Server Error");
+    }
 
+    // res.json([])
+  }
+);
 
-
-module.exports = router
+module.exports = router;
