e71e63f116b6e02f828067213cc6f6c66d6971b1
Neeraj Sharma <neerajkumarsharma279@gmail.com>
2023-03-01 17:49:43 -0500

    resolve and error in auth.js
    resolve and error in auth.js

--- React_Course/inotebook/backend/routes/auth.js
+++ React_Course/inotebook/backend/routes/auth.js
@@ -111,7 +111,7 @@ router.post('/login',[
 router.post('/getuser', fetchuser, async(req,res)=>{
 
 try {
-  userId=req.user.id;
+  let userId=req.user.id;
   const user= await User.findById(userId).select("-password")
   res.send(user)
 } 
