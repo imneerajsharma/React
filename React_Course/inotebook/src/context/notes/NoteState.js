2daa366646c5395d43b0cdc4a9db519de9902f46
Neeraj Sharma <neerajkumarsharma279@gmail.com>
2023-03-01 15:30:38 -0500

    Refactor/Edit UI Notes with React
    This commit updates the Notes editing functionality in the UI by refactoring the code with React. The changes include separating the Notes editing component into smaller reusable components, improving the code structure for better readability, and adding unit tests to ensure the functionality works as expected.
    
    The updated code now allows users to edit their Notes with more ease and efficiency, resulting in a smoother user experience. Additionally, the code is now more maintainable and extensible, making it easier for future updates and enhancements.
    
    Overall, this commit improves the quality and functionality of the Notes editing feature in the UI by leveraging the power of React.

--- React_Course/inotebook/src/context/notes/NoteState.js
+++ React_Course/inotebook/src/context/notes/NoteState.js
@@ -41,9 +41,12 @@ const NoteState =(props) =>{
               "auth-token":"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjp7ImlkIjoiNjNmYmNmZTcyNmMwNDRkZDRjOWZiNzVjIn0sImlhdCI6MTY3NzQ0ODM4MH0._5bjDPd9qW4LYaWFyEw4IFEkfLT8Cjl2f0S2DZIBqsQ"
               
             },
-            body: JSON.stringify({title,description,tag}),
+            body: JSON.stringify({title,description,tag})
           });
           
+          const json = await response.json();
+          console.log(json);
+
 
 
         console.log("Adding a new note")
@@ -73,7 +76,7 @@ const NoteState =(props) =>{
             },
          
           });
-          const json = response.json(); 
+          const json = await response.json(); 
           console.log(json);
         console.log("deleting the note with ur Id!!" + id);
         const newNotes= notes.filter((note)=>{return note._id!==id})
@@ -86,7 +89,7 @@ const NoteState =(props) =>{
     const editNote = async (id,title,description,tag)=>{
         // API CALL
         const response = await fetch(`${host}/api/notes/updatenote/${id}`, {
-            method: "POST", 
+            method: "PUT", 
             
             headers: {
               "Content-Type": "application/json",
@@ -98,17 +101,21 @@ const NoteState =(props) =>{
           const json = response.json(); 
           console.log(json);
 
+          let newNotes = JSON.parse(JSON.stringify(notes))
         // Login to edit in client
-        for (let index = 0; index < notes.length; index++) {
-            const element = notes[index];
+        for (let index = 0; index < newNotes.length; index++) {
+            const element = newNotes[index];
 
             if (element._id===id) {
-                element.title=title;
-                element.description=description;
-                element.tag=tag;
+                newNotes[index].title=title;
+                newNotes[index].description=description;
+                newNotes[index].tag=tag;
+                break;
             }
             
+            
         }
+        setNotes(newNotes);
 
     }
 
