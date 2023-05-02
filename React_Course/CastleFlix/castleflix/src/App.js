ac69218429f78407f5326ec6b5e019c9a2d3e2d4
Neeraj Sharma <neerajkumarsharma279@gmail.com>
2023-05-02 17:34:27 -0400

    Update UI with Navbar, Navigation Menu, Login Button, and Descriptions
    - Added a navbar with the Disney logo to the top of the page
    - Set up the navigation menu with the Home icon and remaining menu options in the header
    - Added a login button for user authentication
    - Added logos image and descriptions to enhance the visual appeal of the page and provide context for users. These changes help users to understand the content and purpose of the website.

--- React_Course/CastleFlix/castleflix/src/App.js
+++ React_Course/CastleFlix/castleflix/src/App.js
@@ -2,11 +2,13 @@ import './App.css';
 import {BrowserRouter as Router, Routes, Route} from "react-router-dom";
 import Login from "./components/Login";
 import Home from './components/Home';
+import Header from './components/Header';
 
 function App() {
   return (
     <div className="App">
       <Router>
+        <Header/>
       <Routes>
           <Route path="/" element={<Login/>}></Route>
           {/* <Route path="/" element={<Home/>} /> */}
