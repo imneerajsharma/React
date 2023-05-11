07667464fc54d6c47bac7db8c55ccf70153efa2f
Neeraj Sharma <neerajkumarsharma279@gmail.com>
2023-05-11 14:59:50 -0400

    .

--- React_Course/MovieFlix/netflix-build-youtube/src/firebase.js
+++ React_Course/MovieFlix/netflix-build-youtube/src/firebase.js
@@ -1,37 +1,25 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import { getFirestore} from "firebase/firestore";
+import { getAuth } from "firebase/auth";
 
-// Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyAGKhmSx657SMKLJDE0Gf-UMZQyim_xtL8",
-  authDomain: "cineflix-13d1c.firebaseapp.com",
-  projectId: "cineflix-13d1c",
-  storageBucket: "cineflix-13d1c.appspot.com",
-  messagingSenderId: "766017466259",
-  appId: "1:766017466259:web:26deac011ae7e5641f8266"
+  apiKey: "AIzaSyC1v1KqXGiFzH98jWc9W8YS8XrQ_PtHX20",
+  authDomain: "netflix-clone-b1738.firebaseapp.com",
+  projectId: "netflix-clone-b1738",
+  storageBucket: "netflix-clone-b1738.appspot.com",
+  messagingSenderId: "45448191515",
+  appId: "1:45448191515:web:194a218edcc1c38e28e2be"
 };
 
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
+
+const firebaseApp = initializeApp(firebaseConfig);
+const db = getFirestore(firebaseApp);
+const auth = getAuth(firebaseApp);
+
+export { auth };
+export default db;
 
 
-// // Import the functions you need from the SDKs you need
-// import { initializeApp } from "firebase/app";
-// // TODO: Add SDKs for Firebase products that you want to use
-// // https://firebase.google.com/docs/web/setup#available-libraries
 
-// // Your web app's Firebase configuration
-// const firebaseConfig = {
-//   apiKey: "AIzaSyC1v1KqXGiFzH98jWc9W8YS8XrQ_PtHX20",
-//   authDomain: "netflix-clone-b1738.firebaseapp.com",
-//   projectId: "netflix-clone-b1738",
-//   storageBucket: "netflix-clone-b1738.appspot.com",
-//   messagingSenderId: "45448191515",
-//   appId: "1:45448191515:web:194a218edcc1c38e28e2be"
-// };
 
-// // Initialize Firebase
-// const app = initializeApp(firebaseConfig);
 
