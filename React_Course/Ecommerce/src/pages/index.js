689d532d7bf662885e6447a11a26ff01b2010a99
Neeraj Sharma <neerajkumarsharma279@gmail.com>
2023-07-25 13:34:07 -0400

    .

---
+++ React_Course/Ecommerce/src/pages/index.js
@@ -0,0 +1,17 @@
+import Head from "next/head";
+import Header from "../components/Header";
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>Amazon 2.0</title>
+      </Head>
+
+     <h1>Hi Neeraj Sharma</h1>
+     <Header/>
+
+     
+    </div>
+  );
+}
