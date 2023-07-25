f59dc56907fb403c4d757183d582fc37dfd90701
Neeraj Sharma <neerajkumarsharma279@gmail.com>
2023-07-25 17:02:58 -0400

    .

--- React_Course/Ecommerce/src/components/ProductFeed.js
+++ React_Course/Ecommerce/src/components/ProductFeed.js
@@ -1,11 +1,27 @@
 import React from 'react'
+import Product from './Product'
 
 function ProductFeed({products}) {
   return (
-    <div>
-      <h1>Products .......</h1>
-      {products.map((product)=>(
-        <p key={product.id}>{product.title}</p>
+    <div className='grid grid-flow-row-dense md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 md:-mt-52 mx-auto'>
+      
+      {products
+      .slice(0,4)
+      .map(({id,title,price,description,category,image})=>(
+        <Product
+            key={id}
+            id={id}
+            title={title}
+            price={price}
+            description={description}
+            category={category}
+            image={image}
+        
+        />
+        
+        
+        
+        // <p key={product.id}>{product.title}</p>
         // <p>{product.title}</p> // this will give an error because 
 
 //  The error "Unhandled Runtime Error: TypeError: Cannot read properties of 
@@ -20,6 +36,43 @@ function ProductFeed({products}) {
 //  making sure that the products prop is available and contains valid data before rendering 
 //  the ProductFeed component.
       
+      ))}
+
+      {/* <img className='md:col-span-full' src="https://links.papareact.com/dyz" alt=""/> */}
+      <img className='md:col-span-full' src="./advertise.jpg" alt=""/>
+      
+      <div className='md:col-span-2'>
+      {products
+      .slice(4,5)
+      .map(({id,title,price,description,category,image})=>(
+        <Product
+            key={id}
+            id={id}
+            title={title}
+            price={price}
+            description={description}
+            category={category}
+            image={image}
+        
+        />
+ 
+      ))}
+      </div>
+
+      {products
+      .slice(5,products.length)
+      .map(({id,title,price,description,category,image})=>(
+        <Product
+            key={id}
+            id={id}
+            title={title}
+            price={price}
+            description={description}
+            category={category}
+            image={image}
+        
+        />
+ 
       ))}
     </div>
   )
